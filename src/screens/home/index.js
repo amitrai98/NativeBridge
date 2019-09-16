557b1b647fb4fcd7bdc7125437562bd8956e5b81
Amit Rai <amit.rai@evontech.com>
2019-09-16 14:41:37 +0530

    circle ci

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import {View, Text} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import ToastModule from "../../util/nativemodules/toastmodule/ToastModule"
 
 type Props = {};
 
@@ -8,7 +10,15 @@ export class Home extends Component<Props> {
     
     return (
       <View>
-       <Text>hello how are you </Text>
+       <Text>hello how are you andrew </Text>
+       <TouchableOpacity
+       onPress={
+         ()=>{
+          ToastModule.show('Awesome', ToastModule.SHORT);
+         }
+       }>
+         <Text>show toast on android</Text>
+       </TouchableOpacity>
       </View>
     );
   }
